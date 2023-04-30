77d8cb626560df410536501774eb83b0a5829ded
UrsL <131500314+UrsL2001@users.noreply.github.com>
2023-04-30 03:05:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,134 @@
+function enviarT() {
+  var a1 = document.getElementById("a1").value;
+  localStorage.setItem("a1", a1);
+  window.location.href = "outra_pagina.html";
+
+   var b1= document.getElementById("b1").value;
+  localStorage.setItem("b1", b1);
+  window.location.href = "outra_pagina.html";
+
+  var c1= document.getElementById("c1").value;
+  localStorage.setItem("c1", c1);
+  window.location.href = "outra_pagina.html";
+
+  var m1= document.getElementById("m1").value;
+  localStorage.setItem("m1", m1);
+  window.location.href = "outra_pagina.html";
+
+// Elemento 2
+var a2 = document.getElementById("a2").value;
+localStorage.setItem("a2", a2);
+window.location.href = "outra_pagina.html";
+
+var b2 = document.getElementById("b2").value;
+localStorage.setItem("b2", b2);
+window.location.href = "outra_pagina.html";
+
+var c2 = document.getElementById("c2").value;
+localStorage.setItem("c2", c2);
+window.location.href = "outra_pagina.html";
+
+
+var a3 = document.getElementById("a3").value;
+localStorage.setItem("a3", a3);
+window.location.href = "outra_pagina.html";
+
+var b3 = document.getElementById("b3").value;
+localStorage.setItem("b3", b3);
+window.location.href = "outra_pagina.html";
+
+var c3 = document.getElementById("c3").value;
+localStorage.setItem("c3", c3);
+window.location.href = "outra_pagina.html";
+
+
+var a4 = document.getElementById("a4").value;
+localStorage.setItem("a4", a4);
+window.location.href = "outra_pagina.html";
+
+var b4 = document.getElementById("b4").value;
+localStorage.setItem("b4", b4);
+window.location.href = "outra_pagina.html";
+
+var c4 = document.getElementById("c4").value;
+localStorage.setItem("c4", c4);
+window.location.href = "outra_pagina.html";
+
+var a5 = document.getElementById("a5").value;
+localStorage.setItem("a5", a5);
+window.location.href = "outra_pagina.html";
+
+var b5 = document.getElementById("b5").value;
+localStorage.setItem("b5", b5);
+window.location.href = "outra_pagina.html";
+
+var c5 = document.getElementById("c5").value;
+localStorage.setItem("c5", c5);
+window.location.href = "outra_pagina.html";
+
+
+var a6 = document.getElementById("a6").value;
+localStorage.setItem("a6", a6);
+window.location.href = "outra_pagina.html";
+
+var b6 = document.getElementById("b6").value;
+localStorage.setItem("b6", b6);
+window.location.href = "outra_pagina.html";
+
+var c6 = document.getElementById("c6").value;
+localStorage.setItem("c6", c6);
+window.location.href = "outra_pagina.html";
+
+
+var a7 = document.getElementById("a7").value;
+localStorage.setItem("a7", a7);
+window.location.href = "outra_pagina.html";
+
+var b7 = document.getElementById("b7").value;
+localStorage.setItem("b7", b7);
+window.location.href = "outra_pagina.html";
+
+var c7 = document.getElementById("c7").value;
+localStorage.setItem("c7", c7);
+window.location.href = "outra_pagina.html";
+
+var a8 = document.getElementById("a8").value;
+localStorage.setItem("a8", a8);
+window.location.href = "outra_pagina.html";
+
+var b8 = document.getElementById("b8").value;
+localStorage.setItem("b8", b8);
+window.location.href = "outra_pagina.html";
+
+var c8 = document.getElementById("c8").value;
+localStorage.setItem("c8", c8);
+window.location.href = "outra_pagina.html";
+
+var a9 = document.getElementById("a9").value;
+localStorage.setItem("a9", a9);
+window.location.href = "outra_pagina.html";
+
+var b9 = document.getElementById("b9").value;
+localStorage.setItem("b9", b9);
+window.location.href = "outra_pagina.html";
+
+var c9 = document.getElementById("a9").value;
+localStorage.setItem("c9", c9);
+window.location.href = "outra_pagina.html";
+
+var a10 = document.getElementById("a10").value;
+localStorage.setItem("a10", a10);
+window.location.href = "outra_pagina.html";
+
+var b10 = document.getElementById("b10").value;
+localStorage.setItem("b10", b10);
+window.location.href = "outra_pagina.html";
+
+var c10 = document.getElementById("c10").value;
+localStorage.setItem("c10", c10);
+window.location.href = "outra_pagina.html";
+
+
+
+}
+
