c84a3762d58159c921f6ff7b1dbf5818899117ab
UrsL <131500314+UrsL2001@users.noreply.github.com>
2023-05-03 16:34:31 -0300

    Update save.js

--- save.js
+++ save.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+  $('form').submit(function() {
+    var textarea_content = $('#a1').val();
+    $.ajax({
+      type: 'POST',
+      url: 'https://bunkerbd.000webhostapp.com/salvar-textarea.php',
+      data: { content: textarea_content }
+    });
+  });
+});
