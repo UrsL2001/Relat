5e9d2b729835ab234ca7a09eb72251fa7c842d02
UrsL <131500314+UrsL2001@users.noreply.github.com>
2023-05-03 14:53:04 -0300

    Update save.js

--- save.js
+++ save.js
@@ -2,7 +2,7 @@
 const savedValue = localStorage.getItem('textareaValue');
 
 // obtém a referência para o textarea
-const textarea = document.querySelector('#a1');
+const a1 = document.querySelector('#a1');
 const b1 = document.querySelector('#b1');
 const c1 = document.querySelector('#c1');
 
