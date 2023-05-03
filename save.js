cdf05d8c5d7c93ee780a4b74bc5ba3c89c4ece89
UrsL <131500314+UrsL2001@users.noreply.github.com>
2023-05-03 17:33:52 -0300

    Update save.js

--- save.js
+++ save.js
@@ -1,23 +1,31 @@
-<script>
-    // Define um objeto para armazenar os valores das textareas
-    var values = {};
+function saveTextareaValues() {
+  var values = {};
+  var textareas = document.getElementsByTagName("textarea");
+  for (var i = 0; i < textareas.length; i++) {
+    var id = textareas[i].id;
+    var value = textareas[i].value;
+    values[id] = value;
+  }
+  localStorage.setItem("textareaValues", JSON.stringify(values));
+}
 
-    // Itera sobre todas as textareas
-    var textareas = document.querySelectorAll("textarea");
-    textareas.forEach(function(textarea) {
-        // Adiciona um event listener no evento "blur" da textarea
-        textarea.addEventListener("blur", function() {
-            // Armazena o valor da textarea no objeto "values"
-            values[textarea.id] = this.value;
-            // Armazena o objeto "values" no localStorage
-            localStorage.setItem("textareaValues", JSON.stringify(values));
-        });
+function loadTextareaValues() {
+  var values = JSON.parse(localStorage.getItem("textareaValues"));
+  if (values) {
+    var textareas = document.getElementsByTagName("textarea");
+    for (var i = 0; i < textareas.length; i++) {
+      var id = textareas[i].id;
+      var value = values[id];
+      if (value) {
+        textareas[i].value = value;
+      }
+    }
+  }
+}
 
-        // Recupera os valores do localStorage e define os valores das textareas
-        var storedValues = JSON.parse(localStorage.getItem("textareaValues"));
-        if (storedValues && storedValues[textarea.id]) {
-            textarea.value = storedValues[textarea.id];
-            values[textarea.id] = storedValues[textarea.id];
-        }
-    });
-</script>
+var textareas = document.querySelectorAll('textarea[id^="a"], textarea[id^="b"], textarea[id^="c"]');
+for (var i = 0; i < textareas.length; i++) {
+  textareas[i].addEventListener("blur", saveTextareaValues);
+}
+
+loadTextareaValues();
