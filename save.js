a99fcd3a34384dff6655e8ca45b2d0b822bb4120
UrsL <131500314+UrsL2001@users.noreply.github.com>
2023-05-03 17:16:20 -0300

    Update save.js

--- save.js
+++ save.js
@@ -1,10 +1,25 @@
-$(document).ready(function() {
-  $('form').submit(function() {
-    var textarea_content = $('#a1').val();
-    $.ajax({
-      type: 'POST',
-      url: 'https://bunkerbd.000webhostapp.com/salvar-textarea.php',
-      data: { content: textarea_content }
+<script>
+    // Adiciona um event listener no evento "beforeunload" do window
+    window.addEventListener("beforeunload", function() {
+        // Cria um cookie com o valor da textarea
+        document.cookie = "a1=" + encodeURIComponent(document.getElementById("a1").value);
     });
-  });
-});
+
+    // Função para recuperar o valor de um cookie
+    function getCookie(name) {
+        var cookieArr = document.cookie.split("; ");
+        for (var i = 0; i < cookieArr.length; i++) {
+            var cookiePair = cookieArr[i].split("=");
+            if (name == cookiePair[0]) {
+                return decodeURIComponent(cookiePair[1]);
+            }
+        }
+        return null;
+    }
+
+    // Recupera o valor do cookie e define o valor da textarea
+    var a1Value = getCookie("a1");
+    if (a1Value) {
+        document.getElementById("a1").value = a1Value;
+    }
+</script>
